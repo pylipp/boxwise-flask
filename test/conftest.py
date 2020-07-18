1e256a0b876e7abb18fa11a9dba9d40a745ab920
pylipp <philipp.metzner@tum.de>
2020-07-18 12:21:30 +0200

    Add unit test for private Flask app endpoint
    Patch the 'requires_auth' decorator to circumvent auth mechanism

--- test/conftest.py
+++ test/conftest.py
@@ -8,21 +8,38 @@ the fixture is defined in the 'conftest' module.
 More details about the mechanism behind fixtures, and predefined fixtures at
 https://docs.pytest.org/en/stable/fixture.html#pytest-fixtures-explicit-modular-scalable
 """
+import functools
 import os
 import tempfile
 
 import pytest
 from playhouse.flask_utils import FlaskDB
 
-from boxwise_flask.routes import app
+from boxwise_flask import auth_helper
 
 
 @pytest.fixture
-def client():
+def client(monkeypatch):
     """Fixture providing a baseline for unit tests that rely on database operations via
     the Flask app. The fixture simulates a client sending requests to the app. Adapted
     from https://flask.palletsprojects.com/en/1.1.x/testing/#the-testing-skeleton.
     """
+
+    def null_decorator(f):
+        """Decorator that has no effect."""
+
+        @functools.wraps(f)
+        def decorated(*args, **kwargs):
+            return f(*args, **kwargs)
+
+        return decorated
+
+    # Patch the 'requires_auth' decorator using a built-in pytest fixture
+    monkeypatch.setattr(auth_helper, "requires_auth", null_decorator)
+
+    # Import the app *after* the patching such that it becomes effective
+    from boxwise_flask.routes import app
+
     # Create a temporary Sqlite database and configure the Flask app to use it
     db_fd, db_filepath = tempfile.mkstemp(suffix=".sqlite3")
     app.config["DATABASE"] = {
--- test/test_app.py
+++ test/test_app.py
@@ -5,3 +5,12 @@ def test_index(client):
     """Verify valid app setup by getting the landing page."""
     rv = client.get("/")
     assert b"This is a landing page" == rv.data
+
+
+def test_private_route(client):
+    rv = client.get("/api/private")
+    assert rv.status_code == 200
+    assert (
+        rv.json["message"]
+        == "Hello from a private endpoint! You need to be authenticated to see this."
+    )
